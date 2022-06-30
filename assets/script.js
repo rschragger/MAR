556a90e24264dbeeabd8c7b536c07396226e5c66
Mina Ghaly <minanabil@msn.com>
2022-06-30 23:15:42 +1000

    youtube searchtag, searchlimit

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
-function keyTry(myApiKey){
-  var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=surfing&key='+myApiKey;
+function keyTry(myApiKey,searchTag,searchLimit){
+  
+  var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults='+searchLimit+'&q=surfing&key=AIzaSyCBymAbMHWPWS-_srOFFs7exEMSYX_hiIg&type=video&part=snippet&q='+searchTag
+  console.log(url);
   fetch(url)
     .then(function (response) {
       return response.json();
@@ -9,4 +11,6 @@ function keyTry(myApiKey){
     })
   }
     ;
-  
+
+  keyTry('',"ladygaga",20);
+    //  
