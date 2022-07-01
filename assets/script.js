564b151a2e65f571904f72600bcdc217242e6734
Mina Ghaly <minanabil@msn.com>
2022-07-01 22:53:39 +1000

    function returns youtube links

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+var videoLinks = [];
+var numberOfvideos = 0;
 function keyTry(myApiKey,searchTag,searchLimit){
   
   var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults='+searchLimit+'&q=surfing&key=AIzaSyCBymAbMHWPWS-_srOFFs7exEMSYX_hiIg&type=video&part=snippet&q='+searchTag
@@ -7,10 +9,25 @@ function keyTry(myApiKey,searchTag,searchLimit){
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      numberOfvideos = data["items"].length;  //gets the number of videos returned
+        //gets the number of videos returned
+
+      //gets the id of the videos then concatinate them with the youtube prefix https://www.youtube.com/watch?v= 
+      //to get a full link then insert the full link into videoLinks array.
+      for (var index = 0; index < numberOfvideos; index++) {
+        videoLinks[index]= "https://www.youtube.com/watch?v="+data["items"][index]["id"]["videoId"];
+      }
+      
+      
     })
   }
     ;
 
-  keyTry('',"ladygaga",20);
-    //  
+  keyTry('yourAPIKey',"ladygaga",20);
+  
+console.log(videoLinks);
+
+
+
+  
+  
