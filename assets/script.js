63c2711bbdfa35e6a76c6323d72191769b14936f
Mina Ghaly <minanabil@msn.com>
2022-07-05 21:25:27 +1000

    lyrics finder added

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var videoLinks = [];
 var numberOfvideos = 0;
 function keyTry(myApiKey,searchTag,searchLimit){
   
-  var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults='+searchLimit+'&q=surfing&key='+myApiKey+'&type=video&part=snippet&q='+searchTag
+  var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults='+searchLimit+'&q=surfing&key=AIzaSyCBymAbMHWPWS-_srOFFs7exEMSYX_hiIg&type=video&part=snippet&q='+searchTag
   console.log(url);
   fetch(url)
     .then(function (response) {
@@ -17,16 +17,42 @@ function keyTry(myApiKey,searchTag,searchLimit){
       for (var index = 0; index < numberOfvideos; index++) {
         videoLinks[index]= "https://www.youtube.com/watch?v="+data["items"][index]["id"]["videoId"];
       }
-      
-      
     })
   }
     ;
-
-  keyTry('myApiKey',"ladygaga",20);
-console.log(videoLinks);
-
-
-
   
-  
+
+
+
+
+
+function lyricsFinder(trackId)
+{
+  var myLink = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id='+trackId+'&apikey=976be22c1d2c0d79345b9f3c25a4da66';
+  fetch(myLink)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.message.body.lyrics.lyrics_body);
+      return data.message.body.lyrics.lyrics_body;
+}
+)}
+
+function trackId(songName){
+var trackId ='http://api.musixmatch.com/ws/1.1/track.search?q_track='+songName+'&s_track_rating=desc&f_lyrics_language=en&apikey=976be22c1d2c0d79345b9f3c25a4da66';
+var idNumber;
+fetch(trackId)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      idNumber = data.message.body.track_list[0].track.track_id;
+      console.log(idNumber);
+      lyricsFinder(idNumber);
+}
+)
+}
+
+trackId("bad romance");
+
