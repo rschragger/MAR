75e72ae0d40ea150524d7e024a377855dcc8a4cc
Artur0705 <artlil420@gmail.com>
2022-07-10 22:02:19 +1000

    Added function to switch to youtube, adding favourites from all services, modified js files"

--- assets/js/clickOnService.js
+++ assets/js/clickOnService.js
@@ -2,13 +2,19 @@ var navDropdown = document.getElementById("nav");
 
 var currentService = "";
 
-navDropdown.addEventListener('click', function (event) {
-    event.stopPropagation();
-    var thisButton = event.target;
-    currentService = thisButton.textContent;
-
-   // window.console.log(currentService);
-localStorage.setItem('currentService',currentService);
-    getTopTenApi(currentService, 'AU');
-
+navDropdown.addEventListener("click", function (event) {
+  event.stopPropagation();
+  var thisButton = event.target;
+  currentService = thisButton.textContent;
+  currentElementIndex = null;
+  // window.console.log(currentService);
+  localStorage.setItem("currentService", currentService);
+  switch (currentService) {
+    case "youtube":
+      keyTry();
+      break;
+    default:
+      getTopTenApi(currentService, "AU");
+      break;
+  }
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,11 @@
-
 // Declaring variables
 var videoLinks = [];
 var numberOfvideos = 0;
-var apiKey = "AIzaSyDP8yc-Z0ZxV-aou3CkADOCBBlob-d79J0";
-
-
 var items;
+var currentElementIndex = null;
 
 // Getting favourites from local storage and displaying into favourites section
-function getFavourites () {
+function getFavourites() {
   var favourites = localStorage.getItem("favourites");
   if (favourites) {
     return JSON.parse(favourites) || {};
@@ -17,79 +14,66 @@ function getFavourites () {
 }
 
 // Functions creating list elements start
-function getFavouriteListElement(title, index) {
-  return (
-    "<li id='" +
-    "favourite-item-" +
-    index +
-    "' class='collection-item col m4 l12'>" +
-    "<span class='title col s10'>" +
-    title +
-    "</span>" +
-    "<a  href='#!' class='secondary-content col s2 remove-action'" +
-    "onclick='" +
-    "onRemoveFacourite(" +
-    index +
-    ")'>" +
-    "<i class='material-icons'>clear</i>" +
-    "</a>" +
-    "</li>"
-  );
+function getFavouriteListElement(title, id) {
+  return `
+    <li id='favourite-item-${id}' class='collection-item col m4 l12 favourite-item' onclick='onClickFavoriteItem(${id})'>
+      <span class='title col s10'>
+        ${title}
+      </span>
+      <a  href='#!' class='secondary-content col s2 remove-action' onclick='onRemoveFacourite(${id})'>
+        <i class='material-icons'>clear</i>
+      </a>
+    </li>
+    `;
 }
 
 function getTopListElement(elementData, index, isActive) {
   var activeClass = "";
   if (isActive) activeClass = " active-favourite-action";
-  return (
-    "<li class='top-list-item' onclick='" +
-    "onClickTopItem(" +
-    index +
-    ")'>" +
-    "<div class='collapsible-header top-list-item-header waves-effect waves-teal'>" +
-    "<div>" +
-    "<span class='top-number'>" +
-    (index + 1) +
-    "</span>" +
-    "<img src='" +
-    elementData.snippet.thumbnails.default.url +
-    "' alt='' width='40' height='40' class='circle'/>" +
-    "<div class='top-list-item-title-box'>" +
-    "<span class='title'>" +
-    elementData.snippet.title +
-    "</span>" +
-    "<p>" +
-    elementData.snippet.channelTitle +
-    "</p>" +
-    "</div>" +
-    "<a href='#!' id='" +
-    "add-favourite-" +
-    index +
-    "' class='secondary-content add-favourite-action" +
-    activeClass +
-    "'" +
-    "onclick='" +
-    "onAddFacourite(" +
-    index +
-    ")'>" +
-    "<i class='material-icons'>favorite</i>" +
-    "</a>" +
-    "</div>" +
-    "</div>" +
-    "<pre class='collapsible-body white-text description'>" +
-    elementData.snippet.description +
-    "</pre>" +
-    "</li>"
-  );
+  return `
+    <li class='top-list-item' onclick='onClickTopItem(${index})'>
+      <div class='collapsible-header top-list-item-header waves-effect waves-teal'>
+        <div>
+          <span class='top-number'>
+            ${index + 1}
+          </span>
+          <img src='${
+            elementData.snippet.thumbnails.default.url
+          }' alt='' width='40' height='40' class='circle'/>
+          <div class='top-list-item-title-box'>
+            <span class='title'>
+              ${elementData.snippet.title}
+            </span>
+            <p>
+              ${elementData.snippet.channelTitle}
+            </p>
+          </div>
+          <a 
+            href='#!' 
+            id='add-favourite-${index}' 
+            class='secondary-content add-favourite-action${activeClass}'
+            onclick='onAddFacourite(${index})'>
+            <i class='material-icons'>favorite</i>
+          </a>
+        </div>
+      </div>
+      <pre class='collapsible-body white-text description'>
+        ${elementData.snippet.description}
+      </pre>
+    </li>
+    `;
 }
 
 // Functions creating list elements end
 
-
 // Function to find the lyrics and display in the description if available
 
 function lyricsFinder(songName, youtubeDescription) {
-  $("#description").text('');
-  var trackId = 'http://api.musixmatch.com/ws/1.1/track.search?q_track=' + songName + '&s_track_rating=desc&f_lyrics_language=en&apikey=976be22c1d2c0d79345b9f3c25a4da66';
+  $("#description").text("");
+  var trackId =
+    "http://api.musixmatch.com/ws/1.1/track.search?q_track=" +
+    songName +
+    "&s_track_rating=desc&f_lyrics_language=en&apikey=976be22c1d2c0d79345b9f3c25a4da66";
   var idNumber;
 
   fetch(trackId)
@@ -98,7 +82,10 @@ function lyricsFinder(songName, youtubeDescription) {
     })
     .then(function (data) {
       idNumber = data.message.body.track_list[0].track.track_id;
-      var myLink = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + idNumber + '&apikey=976be22c1d2c0d79345b9f3c25a4da66';
+      var myLink =
+        "http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" +
+        idNumber +
+        "&apikey=976be22c1d2c0d79345b9f3c25a4da66";
       fetch(myLink)
         .then(function (response) {
           return response.json();
@@ -106,36 +93,39 @@ function lyricsFinder(songName, youtubeDescription) {
         .then(function (data) {
           console.log(data.message.body.lyrics.lyrics_body);
           $("#description").text(data.message.body.lyrics.lyrics_body);
-
-
-        }).catch(function() {
-          $("#description").text(youtubeDescription);
         })
-
-    }).catch(function() {
-      $("#description").text(youtubeDescription);
+        .catch(function () {
+          $("#description").text(youtubeDescription);
+        });
     })
-
-
+    .catch(function () {
+      $("#description").text(youtubeDescription);
+    });
 }
 // Function to display youtube window
 
-var currentElementIndex = null;
 function onClickTopItem(index) {
   if (currentElementIndex === null || currentElementIndex !== index) {
     currentElementIndex = index;
     //need to allow for chartmetric or youtube data
-    if(currentService=='youtube'||currentService==''){//youtube
-    var element = items[index];
-    lyricsFinder(element.snippet.title, element.snippet.description)
+    if (currentService == "youtube" || currentService == "") {
+      //youtube
+      var element = items[index];
+      lyricsFinder(element.snippet.title, element.snippet.description);
 
-    $("#player").attr("src", `https://www.youtube.com/embed/${element.id}`);}
-    else{ //chartmetric services
+      $("#player").attr("src", `https://www.youtube.com/embed/${element.id}`);
+    } else {
+      //chartmetric services
       var element = cityStore.obj.data[index];
-    lyricsFinder(element.name, "Sorry, we don't seem to have lyrics yet for " + element.name + " by " + element.artist_names[0]);
+      lyricsFinder(
+        element.name,
+        "Sorry, we don't seem to have lyrics yet for " +
+          element.name +
+          " by " +
+          element.artist_names[0]
+      );
 
-    $("#player").attr("src", `https://www.youtube.com/embed/${element.id}`);
-
+      // $("#player").attr("src", `https://www.youtube.com/embed/${element.id}`);
     }
   }
 }
@@ -143,50 +133,106 @@ function onClickTopItem(index) {
 // Ability to add favourites to local storage
 function onAddFacourite(index) {
   var favourites = getFavourites();
-  var item = items[index];
+  var currentService = localStorage.getItem("currentService");
 
-  if (!favourites[index]) {
-    favourites[index] = items[index];
-    localStorage.setItem("favourites", JSON.stringify(favourites));
+  if (currentService === "youtube") {
+    var currentList = items;
+    var item = currentList[index];
 
-    $("#favourites").append(getFavouriteListElement(item.snippet.title, index));
-    $("#add-favourite-" + index).addClass("active-favourite-action");
+    if (!favourites[item.id]) {
+      favourites[item.id] = {
+        index: index,
+        id: item.id,
+        name: item.snippet.title,
+        serviceType: "youtube",
+      };
+      localStorage.setItem("favourites", JSON.stringify(favourites));
+      $("#favourites").append(
+        getFavouriteListElement(item.snippet.title, item.id)
+      );
+      $("#add-favourite-" + index).addClass("active-favourite-action");
+    }
+  } else {
+    var currentList = cityStore.obj.data;
+    var item = currentList[index];
+
+    if (!favourites[item.id]) {
+      favourites[item.id] = {
+        index: index,
+        id: item.id,
+        name: item.name,
+        serviceType: currentService,
+      };
+      localStorage.setItem("favourites", JSON.stringify(favourites));
+      $("#favourites").append(getFavouriteListElement(item.name, item.id));
+      $("#add-favourite-" + index).addClass("active-favourite-action");
+    }
   }
 }
-{/* <li id="favourite-item-0" class="collection-item col m4 l12"><span class="title col s10">周杰倫 Jay Chou【最偉大的作品 Greatest Works of Art】Official MV</span><a href="#!" class="secondary-content col s2 remove-action" onclick="onRemoveFacourite(0)"><i class="material-icons">clear</i></a></li> */}
+{
+  /* <li id="favourite-item-0" class="collection-item col m4 l12"><span class="title col s10">周杰倫 Jay Chou【最偉大的作品 Greatest Works of Art】Official MV</span><a href="#!" class="secondary-content col s2 remove-action" onclick="onRemoveFacourite(0)"><i class="material-icons">clear</i></a></li> */
+}
+
+function onClickFavoriteItem(id) {
+  var favourites = getFavourites();
+  var item = favourites[id];
+  var itemIndex = item;
+
+  if (currentElementIndex === null || currentElementIndex !== index) {
+    currentElementIndex = itemIndex;
+
+    if (item.serviceType == "youtube") {
+      //youtube
+      var element = items[itemIndex];
+      lyricsFinder(item.name, element.snippet.description);
+
+      $("#player").attr("src", `https://www.youtube.com/embed/${item.id}`);
+    } else {
+      //chartmetric services
+      var element = cityStore.obj.data[itemIndex];
+      lyricsFinder(
+        item.name,
+        "Sorry, we don't seem to have lyrics yet for " +
+          item.name +
+          " by " +
+          element.artist_names[0]
+      );
+
+      // $("#player").attr("src", `https://www.youtube.com/embed/${element.id}`);
+    }
+  }
+}
 
 // Deleting favourites from list
-function onRemoveFacourite(index) {
+function onRemoveFacourite(id) {
   var favourites = getFavourites();
-  delete favourites[index];
+  var itemIndex = favourites[id].index;
+  delete favourites[id];
 
   localStorage.setItem("favourites", JSON.stringify(favourites));
 
-  $("#favourite-item-" + index).remove();
-  $("#add-favourite-" + index).removeClass("active-favourite-action");
+  $("#favourite-item-" + id).remove();
+  $("#add-favourite-" + itemIndex).removeClass("active-favourite-action");
 }
 
 // Making a fetch request to display top 10 videos
-var maxResult = 10;
-var url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=${maxResult}&regionCode=AU&videoCategoryId=10&key=${apiKey}`;
-console.log(url);
-fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    var favourites = getFavourites();
-    items = data.items;
-    $("#top-list-container").innerHTML = ''; //reset old list to nothing
-
-    for (var index = 0; index < items.length; index++) {
-      var elementData = items[index];
-      var listItem = getTopListElement(elementData, index, favourites[index]);
-      $("#top-list-container").append(listItem);
-    }
-  });
 
 $(document).ready(function () {
+  var currentService = localStorage.getItem("currentService");
+
+  switch (currentService) {
+    case null:
+      localStorage.setItem("currentService", "youtube");
+      keyTry();
+      break;
+    case "youtube":
+      keyTry();
+      break;
+    default:
+      getTopTenApi(currentService, "AU");
+      break;
+  }
+
   // Dropdown menu functionality and making lists collapsible
   $(".dropdown-trigger").dropdown();
   $(".collapsible").collapsible();
@@ -204,11 +250,10 @@ $(document).ready(function () {
 
   // Displays favourites from local storage when reloading the page
   var favourites = getFavourites();
-  var favouritesValues = Object.values(favourites);
+  var favouritesKeys = Object.keys(favourites);
 
-  for (var index = 0; index < favouritesValues.length; index++) {
-    var item = favouritesValues[index];
-    $("#favourites").append(getFavouriteListElement(item.snippet.title, index));
+  for (var index = 0; index < favouritesKeys.length; index++) {
+    var key = favouritesKeys[index];
+    $("#favourites").append(getFavouriteListElement(favourites[key].name, key));
   }
 });
-
